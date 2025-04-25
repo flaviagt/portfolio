4b6201b15086ca63e4cfc584c6858dd480bb21c5
flaviagt <ftedjarutjianta@ucsd.edu>
2025-04-25 13:22:15 -0700

    fixed projects

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 async function initLatestProjects() {
-    const projects = await fetchJSON('./lib/projects.json');
+    const projects = await fetchJSON('../lib/projects.json');
     const latestProjects = projects.slice(0, 3);
     const projectsContainer = document.querySelector('.projects');
     renderProjects(latestProjects, projectsContainer, 'h2');
