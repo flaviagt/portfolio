8b0245acca148bac69a88fe89e153d38c61e3e63
flaviagt <ftedjarutjianta@ucsd.edu>
2025-04-25 13:40:39 -0700

    change for latest proj

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 async function initLatestProjects() {
-    const projects = await fetchJSON('lib/projects.json');
+    const projects = await fetchJSON('./lib/projects.json');
     const latestProjects = projects.slice(0, 3);
     const projectsContainer = document.querySelector('.projects');
+    if (!projectsContainer) {
+        onsole.error('No .projects container found on the homepage.');
+        return;
+    }
     renderProjects(latestProjects, projectsContainer, 'h2');
   
     const githubData = await fetchGitHubData('flaviagt');
