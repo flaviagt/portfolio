21424f91da1db6e80f8cf2a8cea70e4f47b37e03
flaviagt <ftedjarutjianta@ucsd.edu>
2025-04-25 13:14:58 -0700

    lab 4 done

--- global.js
+++ global.js
@@ -94,6 +94,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   });
 }
 
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
 let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 async function initLatestProjects() {
-  const projects = await fetchJSON('./lib/projects.json');
-  const latestProjects = projects.slice(0, 3);
-  const projectsContainer = document.querySelector('.projects');
-  renderProjects(latestProjects, projectsContainer, 'h2');
-}
-initLatestProjects();
+    const projects = await fetchJSON('./lib/projects.json');
+    const latestProjects = projects.slice(0, 3);
+    const projectsContainer = document.querySelector('.projects');
+    renderProjects(latestProjects, projectsContainer, 'h2');
+  
+    const githubData = await fetchGitHubData('flaviagt');
+    const profileStats = document.querySelector('#profile-stats');
+    if (profileStats) {
+      profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+    }
+  }
+  initLatestProjects();
