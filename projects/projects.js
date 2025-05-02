c3dead778721057f0be7a2eaaabdf19ee67e9f13
flaviagt <ftedjarutjianta@ucsd.edu>
2025-05-02 16:39:41 -0700

    until step 1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
 async function initProjects() {
@@ -5,8 +6,36 @@ async function initProjects() {
   const projectsContainer = document.querySelector('.projects');
   const projectsTitle = document.querySelector('.projects-title');
   if (projectsTitle) {
-    projectsTitle.textContent = `${projects.length} Projects`;
+    projectsTitle.textContent = `Projects (${projects.length})`;
   }
   renderProjects(projects, projectsContainer, 'h2');
+
+  // D3 pie chart
+  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+  let data = [1, 2];
+  let total = 0;
+
+  for (let d of data) {
+    total += d;
+  }
+
+  let angle = 0;
+  let arcData = [];
+
+  for (let d of data) {
+    let endAngle = angle + (d / total) * 2 * Math.PI;
+    arcData.push({ startAngle: angle, endAngle });
+    angle = endAngle;
+  }
+
+  let arcs = arcData.map((d) => arcGenerator(d));
+  let colors = ['gold', 'purple'];
+
+  arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors[idx]);
+  });
 }
 initProjects();
